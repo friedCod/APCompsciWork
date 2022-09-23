c2d87c2697927aa830e104808ebc801ee77faa47
iiiiisabelle <isabellefriedlich25@marlborough.org>
2022-09-22 21:00:29 -0700

    fibonacci

---
+++ Unit1/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci
+{
+    public static void main(String[] args)
+    {
+int a=0;
+int b=0;
+int c=1;
+for(int i=0; i<20; i++){
+  System.out.println(c);
+  a = b;
+  b = c;
+  c = a+b;
+
+}
+  }
+    }
