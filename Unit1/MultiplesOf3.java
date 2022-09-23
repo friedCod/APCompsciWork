37912413834892a389996f8c3ee7bf7530e2567a
iiiiisabelle <isabellefriedlich25@marlborough.org>
2022-09-22 19:02:29 -0700

    multiples of 3

--- Unit1/MultiplesOf3.java
+++ Unit1/MultiplesOf3.java
@@ -2,10 +2,19 @@ public class MultiplesOf3
 {
     public static void main(String[] args)
     {
-for(int n=0; n<20; n++){
+int n = 3;
+for(int i=0; i<30; i++){
+System.out.println(n);
+n += 3;
+}
+  }
+    }
+
+
+
+
+/* for(int n=0; n<20; n++){
   if(n%3 == 0){
 System.out.println(n);
   }
-  }
-}
-    }
+  */
