b6879ad60a41b82e11dc08a538168e351768bf06
iiiiisabelle <isabellefriedlich25@marlborough.org>
2022-12-15 08:04:16 -0800

    .

--- Unit7/CountLucky.java
+++ Unit7/CountLucky.java
@@ -1,11 +1,11 @@
 class CountLucky{
 
-  public static void TestCountLucky(double[] values, int expected){
+  public static void TestCountLucky(int[] values, int expected){
 
-    double result = CountLucky(values);
+    int result = CountLucky(values);
 
     System.out.println("values: ");
-    for(double value : values){
+    for(int value : values){
       System.out.println(value + " ");
     }
 
@@ -18,11 +18,11 @@ class CountLucky{
 
   }
 
-  public static double CountLucky(double[] values){
+  public static int CountLucky(int[] values){
 
-    double result = 0;
+    int result = 0;
 
-    for(double value : values){
+    for(int value : values){
       if((value%7==0)||((value-7)%10==0)){
         result = result+1;
       }
@@ -32,19 +32,23 @@ class CountLucky{
 
   public static void main(String[] args){
 
-    double[] values = new double[4];
+    int[] values = new int[4];
     values[0] = 7;
     values[1] = 14;
     values[2] = 27;
     values[3] = 5;
 
-    double[] values2 = new double[3];
+    int[] values2 = new int[3];
     values2[0] = 64937;
     values2[1] = 0;
     values2[2] = 3;
 
+    int[] values3 = {7,13,17};
+
     TestCountLucky(values, 3);
     TestCountLucky(values2, 2);
+    TestCountLucky(values3, 2);
+
 
   }
 }
