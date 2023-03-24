ddc1b79efa7f3d02c065506fc078de5826607d5e
iiiiisabelle <isabellefriedlich25@marlborough.org>
2023-03-23 21:04:25 -0700

    unit13

---
+++ Unit12/AcademicClass.java
@@ -0,0 +1,90 @@
+import java.util.*;
+public class AcademicClass{
+
+  private Teacher teacher;
+  private ArrayList<Student> students;
+
+  public AcademicClass(Teacher teacher){
+
+    this.teacher = teacher;
+    students = new ArrayList<Student>();
+
+  }
+
+  public void addStudent(Student student){
+
+    students.add(student);
+
+  }
+
+  public void printInfo(){
+
+    System.out.println("Teacher name: " + teacher.getName());
+    System.out.println();
+    System.out.print("Student names: ");
+    for(Student s : students){
+
+      System.out.print(s.getName() + " ");
+
+    }
+  }
+
+  public ArrayList<String> favoriteNumber(int n){
+
+    ArrayList<String> result = new ArrayList<String>();
+
+    for(Student s: students){
+      if(s.getFavorite() == n){
+        result.add(s.getName());
+      }
+    }
+
+    return result;
+
+  }
+
+  public ArrayList<String> oddFavorite(){
+
+    ArrayList<String> result = new ArrayList<String>();
+
+    for(Student s: students){
+      if((s.getFavorite() % 2) > 0){
+        result.add(s.getName());
+      }
+    }
+
+    return result;
+
+  }
+
+  public static void main(String[] args){
+
+    Teacher teacher1 = new Teacher("Dr. Kessner", "APCS");
+    Student student1 = new Student("Andrew", 4);
+    Student student2 = new Student("Kai", 7);
+    Student student3 = new Student("Sally", 15);
+    Student student4 = new Student("Viktor", 7);
+    AcademicClass apcs = new AcademicClass(teacher1);
+
+    apcs.addStudent(student1);
+    apcs.addStudent(student2);
+    apcs.addStudent(student3);
+    apcs.addStudent(student4);
+
+    apcs.printInfo();
+
+    System.out.println();
+
+    for(String n: apcs.favoriteNumber(7)){
+      System.out.print(n + " ");
+    }
+
+    System.out.println();
+
+    for(String o: apcs.oddFavorite()){
+      System.out.print(o + " ");
+    }
+
+  }
+
+}
---
+++ Unit12/Student.java
@@ -0,0 +1,14 @@
+public class Student{
+
+  public Student(String name, int favorite){
+    this.name = name;
+    this.favorite = favorite;
+  }
+
+  public String getName(){return name;}
+  public int getFavorite(){return favorite;}
+
+  private String name;
+  private int favorite;
+
+}
